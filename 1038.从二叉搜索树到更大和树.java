e81c9e1fed11b48493e5061cbaf35e54ace1dd14
yida.hu <huyidada@gmail.com>
2022-03-25 00:45:02 +0800

    lc

---
+++ 1038.从二叉搜索树到更大和树.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=1038 lang=java
+ *
+ * [1038] 从二叉搜索树到更大和树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    public void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.right);
+        sum += root.val;
+        root.val = sum;
+        traverse(root.left);
+    }
+}
+// @lc code=end
+
---
+++ 230.二叉搜索树中第k小的元素.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=230 lang=java
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+
+    int rank = 0;
+    int res = Integer.MIN_VALUE;
+
+    public int kthSmallest(TreeNode root, int k) {
+        traverse(root, k);
+        return res;
+    }
+
+    public void traverse(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left, k);
+        rank++;
+        if (k == rank) {
+            res = root.val;
+            return;
+        }
+        traverse(root.right, k);
+    }
+}
+// @lc code=end
+
